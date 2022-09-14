07f1b1e6ed36ada042d1a9f3ced30cf18176ef4d
Mohamed Hamdan <ahmedhamdan672@gmail.com>
2022-09-14 10:35:29 +0000

    Added shell scripts for generation and viewing

---
+++ fwc_matrix/compile_view.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ext="_assignment"
+dir=$1$ext
+echo $dir
+python3 $dir/codes/$dir.py
+cd $dir
+texfot pdflatex $dir.tex
+cd ..
+cp $dir/$dir.pdf /sdcard/fwc_termux/
+termux-open /sdcard/fwc_termux/$dir.pdf
