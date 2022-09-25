b8eb68f4d551efa71bc9cef827b6888d03b74ab6
Muhammed-Hamdan <ahmedhamdan672@gmail.com>
2022-09-25 17:35:55 +0000

    Added miscellaneous

---
+++ miscellaneous/conic_normal/codes/main.py
@@ -0,0 +1,20 @@
+from sympy import *
+x = Symbol('x')
+# m is the vector perpendicular to normal chord ie m^tx = c
+m = Matrix([-x, 1])
+omat = Matrix(([0, -1], [1, 0]))
+# n is the vector along the normal chord ie h + kn = x 
+n = omat*m
+# Conic parameters
+V = Matrix(([Rational(1,24).limit_denominator(10**8), 0],[0, Rational(-1,18).limit_denominator(10**8)]))
+u = Matrix([0, 0])
+f = Matrix([-1])
+# Point from which normal drawn
+h = Matrix([0, 7*sqrt(3)])
+# Equation solving
+eq1 = n.T*((V*h + u)*(V*h + u).T - (h.T*V*h + 2*u.T*h + f)[0,0]*V)*n
+eq2 = (m.T*V*n)**2
+eq3 = ((V*h + u).T*(n*(m.T*V*n) - m*(n.T*V*n)))**2
+eq = eq1[0,0]*eq2[0,0] - eq3[0,0]
+print(expand(eq))
+print(solveset(eq, x))
