a126c1430f6aa8c1d20a46bec7120440b5797ab3
Muhammed-Hamdan <ahmedhamdan672@gmail.com>
2022-12-19 00:58:33 +0530

    communication: ch1,2,4,5 codes done

--- communication/codes/chapter2/function_1.py
+++ communication/codes/chapter2/function_1.py
@@ -6,7 +6,7 @@ maxrange = 100
 x = np.linspace(-1, maxlim, maxrange) #points on x-axis
 simlen = int(1e6) #number of samples
 cdf = [] #declaring probability list
-uni_randvar = np.loadtxt('uni.dat',dtype='double')
+uni_randvar = np.loadtxt('../rvsample/uni.dat',dtype='double')
 tran_randvar = -2*np.log(1-uni_randvar)
 
 for i in range(0,maxrange):
